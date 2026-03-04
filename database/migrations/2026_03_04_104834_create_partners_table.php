7055298eb9d1c29534992293865008daa2b9c561
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-03-04 12:12:58 +0100

    előkészületek a feladat megoldásához, adatbázistáblák létrehozása

---
+++ database/migrations/2026_03_04_104834_create_partners_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Man;
+use App\Models\Woman;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('man_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('woman_id')->constrained()->cascadeOnDelete();
+            $table->string('dance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partners');
+    }
+};
